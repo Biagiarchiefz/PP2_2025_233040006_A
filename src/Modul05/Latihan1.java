54382e9eb6fa9363289e117afb14a970df5d4e2e
Biagiarchiefz <biagiiarchie@gmail.com>
2025-11-02 18:39:27 +0700

    add latihan 1

---
+++ src/Modul05/Latihan1.java
@@ -0,0 +1,23 @@
+package Modul05;
+
+import javax.swing.*;
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // membuat object Hframe
+                JFrame frame = new JFrame("Jendela pertamaku");
+
+                // atur ukuran jendela ( lebar 400, tinggi 300 )
+                frame.setSize(400, 300);
+
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                // membuat jendelanya terlihat
+                frame.setVisible(true);
+            }
+        });
+    }
+}
